d2d7f363ecbdb2df511b4c58476a1b88be8e38af
James Thorne <james@dtscc.net>
2013-04-02 10:06:17 -0600

    Website mostly done

--- script.js
+++ script.js
@@ -59,8 +59,74 @@ $(function() {
    $(window).resize(handleResize);
    handleResize();
 
+   var openPanel = function(panelName, duration) {
+      if (!duration)
+      {
+         duration = 400;
+      }
+
+      $(".content-body").hide();
+      $("#content-" + panelName).show();
+
+      $("#content").fadeIn(duration);
+
+      //location.replace(('' + window.location).split("#")[0] + "#!" + panelName);
+      window.location.hash = "!" + panelName;
+   };
+
+   var closePanel = function() {
+      $("#content").fadeOut();
+      window.location.hash = "";
+   };
+
+   var getCurrentHashRequest = function() {
+      if (window.location.hash && window.location.hash.substring(0, 2) == "#!")
+      {
+         var hashRequest = window.location.hash.substring("#!".length);
+
+         if ($("#content-" + hashRequest).length > 0)
+         {
+            return hashRequest;
+         }
+      }
+      return "";
+   }
+
+   var checkHash = function() {
+      var request = getCurrentHashRequest();
+
+      if (request == "")
+      {
+         closePanel();
+      }
+      else
+      {
+         openPanel(request);
+      }
+   }
+
    $(window).bind("load", function() {
       isLoaded = true;
       clearWhiteout();
+
+      $(".go-back").bind("click", function() {
+         closePanel();
+      });
+
+      var callback = $(".panel").bind("click", function(e) {
+         var source = this.id;
+         var panelName = source.substring("panel-".length);
+
+         openPanel(panelName);
+      });
+
+      if (window.location.hash && window.location.hash.substring(0, 2) == "#!")
+      {
+         checkHash();
+      }
+
+      window.onhashchange = function() {
+         checkHash();
+      };
    });
 });
